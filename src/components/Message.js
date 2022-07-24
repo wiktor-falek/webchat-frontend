42b54e7e6685b66c7bc9dd15196dee917f2a76ad
wiktor-falek <falekwiktor@gmail.com>
2022-07-24 19:30:51 +0200

    add socketid attribute to message__name

--- src/components/Message.js
+++ src/components/Message.js
@@ -2,7 +2,7 @@ const Message = (props) => {
   return (
     <div className="message">
       <p>
-        <span className="message__name" style={{color: props.color}}>{props.name} </span>
+        <span className="message__name" socketid={props.socketid} style={{color: props.color}}>{props.name} </span>
         <span className="message__timestamp">{props.timestamp}</span>
       </p>
       <p className="message__content">{props.content}</p>
